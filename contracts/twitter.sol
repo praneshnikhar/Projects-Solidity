2496cb785970c08b3ed1000efdc9eda1fedb16c4
praneshnikhar <praneshnikhar@gmail.com>
2025-05-24 00:02:22 +0530

    commit

--- contracts/twitter.sol
+++ contracts/twitter.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.18;
 
 contract twitter {
-    mapping(address => string) public tweets;
+    mapping(address => string[]) public tweets;
 
     function createTweet(string memory _tweet) public {
-        tweets[msg.sender]= _tweet;
+        tweets[msg.sender].push(_tweet);
     }
 
-    function getTweet(address _owner) public view returns(string memory){
+    function getTweet(address _owner, uint _i) public view returns(string memory){
+        return tweets[_owner][_i];
+    }
+    function getAllTweets(address _owner) public view returns(string[] memory){
         return tweets[_owner];
     }
 
