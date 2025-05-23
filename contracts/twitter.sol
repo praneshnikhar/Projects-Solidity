748c927942a3ec72d474bc62d3513f698791f0df
praneshnikhar <praneshnikhar@gmail.com>
2025-05-23 23:32:09 +0530

    commit

--- contracts/twitter.sol
+++ contracts/twitter.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.18;
 
-contract twitter{
+contract twitter {
     mapping(address => string) public tweets;
 
     function createTweet(string memory _tweet) public {
         tweets[msg.sender]= _tweet;
+    }
+
+    function getTweet(address _owner) public view returns(string memory){
+        return tweets[_owner];
     }
 
 
