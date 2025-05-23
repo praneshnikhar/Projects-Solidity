c77cdd15536d7bd2a9ea3380edf54408e77b309a
praneshnikhar <praneshnikhar@gmail.com>
2025-05-23 23:24:41 +0530

    commit

--- contracts/mapping.sol
+++ contracts/mapping.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.18;
 
 contract Mapping {
-    // mapping(address => uint) public myMap;
+    mapping(address => uint) public myMap;
 
-    // function get(address _addr) public view returns (uint){
-    //     return myMap[_addr];
-    // }
+    function get(address _addr) public view returns (uint){
+        return myMap[_addr];
+    }
 
-    // function set(address _addr, uint _i) public{
-    //     myMap[_addr] = _i;
-    // }
+    function set(address _addr, uint _i) public{
+        myMap[_addr] = _i;
+    }
 
-    // function remove(address _addr) public{
-    //     delete myMap[_addr];
-    // }
+    function remove(address _addr) public{
+        delete myMap[_addr];
+    }
 }
