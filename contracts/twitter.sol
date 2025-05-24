e78fc82e78b08202760383e0934bd3f4aead0090
praneshnikhar <praneshnikhar@gmail.com>
2025-05-24 11:44:09 +0530

    commit

--- contracts/twitter.sol
+++ contracts/twitter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 contract twitter {
-
+    uint16 constant MAX_TWEET_LENGTH = 280;
 
     struct Tweet{
         address author;
@@ -14,6 +14,10 @@ contract twitter {
     mapping(address => Tweet[]) public tweets;
 
     function createTweet(string memory _tweet) public {
+       //conditional
+       //if tweet length <=200 then ok or revert
+       require(bytes(_tweet).length<=MAX_TWEET_LENGTH ,"Tweet is too long !" );
+       
         Tweet memory newTweet = Tweet({
             author:msg.sender,
             content:_tweet,
